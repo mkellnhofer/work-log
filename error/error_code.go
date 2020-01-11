d4faecb39f9acadd7e7e7030edc66a0506df1969
Matthias Kellnhofer <matthias@kellnhofer.com>
2020-01-11 01:43:05 +0100

    Add error model struct

---
+++ error/error_code.go
@@ -0,0 +1,15 @@
+package error
+
+const (
+	// Authentication errors
+	AuthUnknown = -100
+
+	// Validation erros
+	ValUnknown = -200
+
+	// Logic errors
+	LogicUnknown = -300
+
+	// System errors
+	SysUnknown = -400
+)

6788b58ac78b8c29c665801e28dd2fb98d60b23e
Matthias Kellnhofer <matthias@kellnhofer.com>
2020-01-12 22:04:11 +0100

    Add router and register middlewares and controllers

--- work_log.go
+++ work_log.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+
 	"kellnhofer.com/work-log/config"
 	"kellnhofer.com/work-log/constant"
 	"kellnhofer.com/work-log/log"
@@ -23,4 +29,45 @@ func main() {
 	db.OpenDb()
 	defer db.CloseDb()
 	db.UpdateDb()
+
+	// Create router
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Create public middleware route
+	pubRoute := negroni.New()
+	pubRoute.Use(init.GetErrorMiddleware())
+	pubRoute.Use(init.GetSessionMiddleware())
+	// Create protected middleware route
+	proRoute := negroni.New()
+	proRoute.Use(init.GetErrorMiddleware())
+	proRoute.Use(init.GetSessionMiddleware())
+	proRoute.Use(init.GetAuthMiddleware())
+
+	// Add public endpoints
+	addEndpoint(router, pubRoute, "GET", "/error", init.GetErrorController().GetErrorHandler())
+	// TODO!!!
+	// Add protected endpoints
+	// TODO!!!
+	// Add resource endpoints
+	// TODO!!!
+
+	// Register router
+	http.Handle("/", router)
+
+	// Start HTTP server
+	log.Infof("Listen on port '%d'.", conf.ServerPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", conf.ServerPort), nil)
+	if err != nil {
+		log.Fatalf("Could not start server! (Error: %s)", err)
+	}
+}
+
+func addEndpoint(r *mux.Router, m *negroni.Negroni, method string, path string, hf http.HandlerFunc) {
+	r.Handle(path, createHandler(m, hf)).Methods(method)
+}
+
+func createHandler(r *negroni.Negroni, h http.HandlerFunc) http.Handler {
+	nr := r.With()
+	nr.UseHandlerFunc(h)
+	return nr
 }

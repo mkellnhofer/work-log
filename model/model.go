9f66761a650431bac970ef1dfca9f83a82ade534
Matthias Kellnhofer <matthias@kellnhofer.com>
2020-08-02 16:15:43 +0200

    Add constants for maximum length of model fields

--- model/model.go
+++ model/model.go
@@ -9,3 +9,14 @@ const (
 	NoSorting   Sorting = 0
 	AscSorting  Sorting = 1
 )
+
+// Maximum string length constants.
+const (
+	MaxLengthRoleName                 = 100
+	MaxLengthUserName                 = 100
+	MaxLengthUserUsername             = 100
+	MaxLengthUserPassword             = 100
+	MaxLengthEntryTypeDescription     = 50
+	MaxLengthEntryActivityDescription = 50
+	MaxLengthEntryDescription         = 200
+)

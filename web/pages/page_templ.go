07af89bcb09ca9fe0c559bc50b7237338a01e54d
Matthias Kellnhofer <matthias@kellnhofer.com>
2024-04-05 20:50:26 +0000

    Update Bootstrap

--- web/pages/page_templ.go
+++ web/pages/page_templ.go
@@ -62,7 +62,7 @@ func Page() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script type=\"text/javascript\" src=\"/js/jquery.min.js\"></script><script type=\"text/javascript\" src=\"/js/popper.min.js\"></script><script type=\"text/javascript\" src=\"/js/bootstrap.min.js\"></script><script type=\"text/javascript\" src=\"/js/app.js\"></script></body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script type=\"text/javascript\" src=\"/js/bootstrap.min.js\"></script><script type=\"text/javascript\" src=\"/js/app.js\"></script></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

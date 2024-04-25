7b9c92a824c9a3b6ff6b145f663e2056a9aafe0c
Matthias Kellnhofer <matthias@kellnhofer.com>
2024-04-25 22:51:09 +0000

    Merge templates

--- web/view/pages/page.go
+++ web/view/pages/page.go
@@ -1,15 +1,9 @@
 package pages
 
 import (
-	"github.com/a-h/templ"
-
 	"kellnhofer.com/work-log/web/view"
 )
 
 func getText(key string) string {
 	return view.GetText(key)
-}
-
-func toURL(url string) templ.SafeURL {
-	return templ.URL(url)
 }
